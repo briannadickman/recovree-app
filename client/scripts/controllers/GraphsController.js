b468c4bc549c891312a7b8a166d7f72a648e23af
Anisa <anisa.ali7@gmail.com>
2017-05-17 15:03:19 -0500

    charts not displaying

--- client/scripts/controllers/GraphsController.js
+++ client/scripts/controllers/GraphsController.js
@@ -1,3 +1,34 @@
-myApp.controller('GraphController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
-  console.log('GraphController sourced!');
+myApp.controller('GraphsController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
+  console.log('GraphsController sourced!');
+
+
+  var context = document.getElementById('skills').getContext('2d');
+  var skillsChart = new Chart(context).Pie(pieData);
+
+  var pieData = [
+     {
+        value: 25,
+        label: 'Java',
+        color: '#811BD6'
+     },
+     {
+        value: 10,
+        label: 'Scala',
+        color: '#9CBABA'
+     },
+     {
+        value: 30,
+        label: 'PHP',
+        color: '#D18177'
+     },
+     {
+        value : 35,
+        label: 'HTML',
+        color: '#6AE128'
+     }
+  ];
+
+
+
+
 }]);
