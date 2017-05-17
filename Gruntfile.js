fc7fd8d2334b0068c4a78e82b0fc6711ab26a9af
Anisa <anisa.ali7@gmail.com>
2017-05-17 18:21:16 -0500

    example graph with chart.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,12 @@ module.exports = function(grunt){
               'angular-animate.min.js',
               'angular-animate.min.js.map'],
         dest: 'server/public/vendors/angular-animate/'
+      },
+      chart: {
+        expand: true,
+        cwd: 'node_modules/chart.js/dist/',
+        src: ['Chart.min.js'],
+        dest: 'server/public/vendors/chartJS/'
       }
     },
     watch: {
--- client/scripts/controllers/GraphsController.js
+++ client/scripts/controllers/GraphsController.js
@@ -2,31 +2,43 @@ myApp.controller('GraphsController', ['$scope', '$http', '$location', 'UserServi
   console.log('GraphsController sourced!');
 
 
-  var context = document.getElementById('skills').getContext('2d');
-  var skillsChart = new Chart(context).Pie(pieData);
-
-  var pieData = [
-     {
-        value: 25,
-        label: 'Java',
-        color: '#811BD6'
-     },
-     {
-        value: 10,
-        label: 'Scala',
-        color: '#9CBABA'
-     },
-     {
-        value: 30,
-        label: 'PHP',
-        color: '#D18177'
-     },
-     {
-        value : 35,
-        label: 'HTML',
-        color: '#6AE128'
-     }
-  ];
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
 
 
 
