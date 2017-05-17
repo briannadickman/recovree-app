252b6d7e539348987a92ab55267f1cd0c023847f
Anisa <anisa.ali7@gmail.com>
2017-05-17 14:28:59 -0500

    conflict fix

--- server/modules/charts.js
+++ server/modules/charts.js
@@ -3,8 +3,7 @@ var express = require('express');
 var Chart = require('chart.js');
 // var myChart = new Chart({...});
 
-<<<<<<< HEAD
-=======
+
 var ctx = document.getElementById("myChart");
 
 
@@ -19,7 +18,6 @@ var ctx = document.getElementById("myChart");
 
 
 
->>>>>>> 3a1a102181c3bd8b4e36f89200a3d5cf4e03e0a8
 
 
 
