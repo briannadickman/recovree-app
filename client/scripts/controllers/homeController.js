99a156d615aa42e8e3f9f001fdc2ef6604448182
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-19 10:26:22 -0500

    tests data persisitance

--- client/scripts/controllers/homeController.js
+++ client/scripts/controllers/homeController.js
@@ -2,10 +2,12 @@ myApp.controller('HomeController', ['$scope', '$http', '$location', 'UserService
   console.log('HomeController sourced!');
   var userObject = UserService.userObject;
 
-  console.log("userObject");
+  console.log("userObject", userObject);
   var refreshSessionObject = UserService.refreshSessionObject;
   refreshSessionObject();
   $scope.sessionObject = UserService.sessionObject;
+  console.log("sessionObject", $scope.sessionObject);
+  console.log("sessionObject.allReflections", $scope.sessionObject.allReflections);
 
   $scope.launchReflection = function(){
     $location.path('/reflection-form/reflect-1');
