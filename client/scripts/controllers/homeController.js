76f3d72b211a2a4d4b14d11cebd8dc125127d755
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-19 10:37:08 -0500

    changes home controller

--- client/scripts/controllers/homeController.js
+++ client/scripts/controllers/homeController.js
@@ -1,13 +1,10 @@
 myApp.controller('HomeController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   console.log('HomeController sourced!');
   var userObject = UserService.userObject;
-
-  console.log("userObject", userObject);
   var refreshSessionObject = UserService.refreshSessionObject;
   refreshSessionObject();
+
   $scope.sessionObject = UserService.sessionObject;
-  console.log("sessionObject", $scope.sessionObject);
-  console.log("sessionObject.allReflections", $scope.sessionObject.allReflections);
 
   $scope.launchReflection = function(){
     $location.path('/reflection-form/reflect-1');
