4c04776d72f1c1e02c123eb2c7e23eba12505fee
Anisa <anisa.ali7@gmail.com>
2017-05-22 14:28:48 -0500

    commented out res.send

--- server/modules/db.js
+++ server/modules/db.js
@@ -4,8 +4,9 @@ var MongoDB = mongoose.connect(mongoURI).connection;
 
 
 // process.env.MONGODB_URI will only be defined if you are running on Heroku
-if(process.env.MONGODB_URI !== undefined) {
+if(process.env.MONGODB_URI) {
     mongoURI = process.env.MONGODB_URI;
+
 } else {
     mongoURI = 'mongodb://localhost:27017/recovree';
 }
@@ -13,7 +14,7 @@ if(process.env.MONGODB_URI !== undefined) {
 //If there is an error connecting to the database, let us know!
 MongoDB.on("error", function(err){
   console.log("Mongo Connection Error :" + err);
-  res.sendStatus(500);
+  //res.sendStatus(500);
 });
 
 //If we successfully hooked up to the database, let us know!
