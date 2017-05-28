0f95b19611de3b69729eeece2244d77327aa328f
Bri Dickman <brianna.dickman@gmail.com>
2017-05-27 20:53:38 -0500

    changes to styling

--- client/scripts/controllers/loginController.js
+++ client/scripts/controllers/loginController.js
@@ -1,6 +1,6 @@
 myApp.controller('LoginController', ['$scope', '$http', '$routeParams', '$location', 'UserService', function($scope, $http, $routeParams, $location, UserService) {
     $scope.user = UserService.user;
-    
+
     $scope.message = '';
     var userObject = UserService.userObject;
 
@@ -55,10 +55,13 @@ myApp.controller('LoginController', ['$scope', '$http', '$routeParams', '$locati
             userObject.userName = response.data.username;
             userObject.id = response.data.id;
             userObject.userType = response.data.userType;
+            console.log(response.data.userType);
             // location works with SPA (ng-route)
             if (userObject.userType === 2){
+              console.log('sending home');
               $location.path('/home');
             } else if (userObject.userType === 1){
+              console.log('sending to admin');
               $location.path('/admin-export');
             }
 
