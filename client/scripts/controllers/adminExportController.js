84e6396ab0618987a833f10abc9ee88545299d82
Anisa <anisa.ali7@gmail.com>
2017-05-28 14:51:47 -0500

    background color, remove label, dummy data for dates/participant count

--- client/scripts/controllers/adminExportController.js
+++ client/scripts/controllers/adminExportController.js
@@ -15,46 +15,45 @@ myApp.controller('AdminExportController', ['$scope', '$http', '$location', 'Admi
   AdminService.countMembers();
   AdminService.countReflectionsByDay();
 
-  // $scope.adminObject = AdminService.adminObject;
-  // console.log('19', $scope.adminObject.memberCount);
 
   //replace with actual dates
-  var days = ['5/16','5/17','5/18', '5/19', '5/20'];
+  var days = ['5/21', '5/22', '5/23', '5/24', '5/25', '5/26', '5/27'];
   //replace with actual count of daily participants
-  var reflectionCount = [8, 6, 10];
+  var reflectionCount = [12, 13, 18, 12, 10, 8, 16];
 
   var ctx4 = document.getElementById("dailyParticipantsChart");
   var dailyParticipantsChart = new Chart(ctx4, {
-    type: 'line',
+    type: 'bar',
     data: {
       labels: days,
-  //     datasets: [{
-  //      fillColor: "rgba(220,220,220,0)",
-  //      strokeColor: "rgba(220,180,0,1)",
-  //      pointColor: "rgba(220,180,0,1)",
-  //      data: [20, 30, 80, 20, 40, 10, 60]
-  //  }, {
-  //      fillColor: "rgba(151,187,205,0)",
-  //      strokeColor: "rgba(151,187,205,1)",
-  //      pointColor: "rgba(151,187,205,1)",
-  //      data: [60, 10, 40, 30, 80, 30, 20]
-  //  }]
-      dataset: [{
-        label: 'Participants',
-        data: [4, 9, 12, 8, 5]
+      datasets: [{
+        label: 'Daily Participants',
+        fill: false,
+        backgroundColor: 'rgba(129, 49, 114, 0.76)',
+        data: reflectionCount
       }]
     },
     options: {
       scales: {
-          yAxes: [{
-              ticks: {
-                  max: 20,
-                  min: 0,
-                  stepSize: 1
-              }
-          }]
+        yAxes: [{
+          ticks: {
+            max: 20,
+            min: 0,
+            stepSize: 1
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem) {
+            return tooltipItem.yLabel;
+          }
+        }
       }
-  }
+    }
   });
 
 }]);
