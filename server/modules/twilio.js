31944866c8e16b5bd509a131ce662b1c7e67127b
Anisa <anisa.ali7@gmail.com>
2017-05-29 19:40:42 -0500

    add other message types

--- server/modules/twilio.js
+++ server/modules/twilio.js
@@ -3,28 +3,35 @@ var cronJob = require('cron').CronJob;
 
 //TWILIO
 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-
-var phoneNumbers = ['6129918411', '7014294214', '6513997345'];
 var twilioNumber = process.env.TWILIO_NUMBER;
-// var messageAlerts = ['Keep up the good work!', 'You are doing great!', 'Wow, look at you go!', 'You are a star!'];
-var messageAlerts = ['This is a friendly reminder to complete your daily Recovree (insert link)',
-  'Find a moment to reflect and complete your daily Recovree (insert link)',
-  'One day at a time. Find time to complete your daily Recovree (insert link)',
-  'Sobriety is a journey. A reminder to complete your daily Recovree (insert link)'
-];
 
+var welcomeMember = 'Welcome to Recovree';
+
+var reminderMessage = ['This is a friendly reminder to complete your daily Recovree (insert link)',
+    'Find a moment to reflect and complete your daily Recovree (insert link)',
+    'One day at a time. Find time to complete your daily Recovree (insert link)',
+    'Sobriety is a journey. A reminder to complete your daily Recovree (insert link)'
+  ];
 var randomIndex = Math.floor(Math.random() * messageAlerts.length);
-var randomMessage = messageAlerts[randomIndex];
-console.log(randomMessage);
+var randomomizeReminderMessage = reminderMessage[randomIndex];
+
+var reflectionCompleteMessage = ['Thank you for completing your daily Recovree!',
+                                'Thanks for making Recovree a part of your day!',
+                                'Many thanks for taking time to complete Recovree today'];
+
+var phoneNumbers = ['6122050534'];
 
 // Using Cron to send automated messages at desiredintervals
 // var textJob = new cronJob( '* * * * *', function(){ // send SMS message every minute
 // for (var i = 0; i < phoneNumbers.length; i++) {
-//   client.messages.create( { to: phoneNumbers[i], from: twilioNumber, body: randomMessage}, function( err, data ) {});
+//   client.messages.create( { to: phoneNumbers[i], from: twilioNumber, body: randomomizeReminderMessage},
+//   function( err, data ) {});
 //   console.log('Text Reminder Sent To: ', phoneNumbers[i]);
 // }
 // console.log('CRON PORTION OF CODE');
 // },  null, true );
+
+
+
 
 module.exports = client;
