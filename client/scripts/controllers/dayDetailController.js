550534ec4427f513fc011a2cba78592e1d751187
Anisa <anisa.ali7@gmail.com>
2017-05-29 21:34:32 -0500

    hide streak graph and replace with quote if not completed reflection, streak =0

--- client/scripts/controllers/dayDetailController.js
+++ client/scripts/controllers/dayDetailController.js
@@ -7,4 +7,24 @@ refreshSessionObject();
 $scope.sessionObject = UserService.sessionObject;
 $scope.launchReflection = UserService.launchReflection;
 
+
+// random message to display when reflection is less than zero or 1
+var reflectionQuote = [
+{
+quote: 'Self-reflection is a humbling process. It is essential to find out why you think, say, and do certain things...then better yourself.',
+ name: '-Sonya Teclai'
+},
+// {
+//   quote:
+//   name:
+// }
+
+     ];
+
+var randomIndex = Math.floor(Math.random() * reflectionQuote.length);
+$scope.randomQuote = reflectionQuote[randomIndex];
+
+console.log($scope.randomQuote);
+
+
 }]);
