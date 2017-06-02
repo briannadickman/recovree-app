b0d9442a23df05e5d8f358b6814e1aeeaaadb233
Anisa <anisa.ali7@gmail.com>
2017-06-02 08:26:37 -0500

    brought in logan's code

--- server/modules/twilio.js
+++ server/modules/twilio.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var cronJob = require('cron').CronJob;
+var Users = require('../models/user');
+var asyncMod = require('async');
 
 //TWILIO
 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
@@ -11,34 +13,35 @@ var newMember = '';
 
 var welcomeMember = 'Welcome to Recovree';
 var reminderMessage = ['This is a friendly reminder to complete your daily Recovree (insert link)',
-  'Find a moment to reflect and complete your daily Recovree (insert link)',
-  'One day at a time. Find time to complete your daily Recovree (insert link)',
-  'Sobriety is a journey. A reminder to complete your daily Recovree (insert link)'
+    'Find a moment to reflect and complete your daily Recovree (insert link)',
+    'One day at a time. Find time to complete your daily Recovree (insert link)',
+    'Sobriety is a journey. A reminder to complete your daily Recovree (insert link)'
 ];
 var randomIndex = Math.floor(Math.random() * reminderMessage.length);
 var randomomizeReminderMessage = reminderMessage[randomIndex];
 var reflectionCompleteMessage = ['Thank you for completing your daily Recovree!',
-                                'Thanks for making Recovree a part of your day!',
-                                'Many thanks for taking time to complete Recovree today'];
+    'Thanks for making Recovree a part of your day!',
+    'Many thanks for taking time to complete Recovree today'
+];
 
-function sendTwilioSMS(phoneNumber, message){
-  client.messages.create( { to: phoneNumber, from: twilioNumber, body: message},
-  function( err, data ) {
-    // if(err) {
-    //   console.log('Error Sending Message');
-    // } else {
-    //   console.log('SMS SENT', data);
-    // }
-  });
+function sendTwilioSMS(phoneNumber, message) {
+    client.messages.create({ to: phoneNumber, from: twilioNumber, body: message },
+        function(err, data) {
+            // if(err) {
+            //   console.log('Error Sending Message');
+            // } else {
+            //   console.log('SMS SENT', data);
+            // }
+        });
 }
 
 
 // Send SMS message 6pm everyday
-function scheduleSMS (params) {
-  // var textJob = new cronJob( '0 18 * * *', function(){ 
- // }
- // console.log('CRON PORTION OF CODE');
- // },  null, true );
+function scheduleSMS(params) {
+    // var textJob = new cronJob( '0 18 * * *', function(){ 
+    // }
+    // console.log('CRON PORTION OF CODE');
+    // },  null, true );
 }
 
 // var textJob = new cronJob( '* * * * *', function(){ 
@@ -51,6 +54,29 @@ function scheduleSMS (params) {
 // },  null, true );
 
 
+//Logan's Code
+var sendReminders = function() {
+    var phoneNumbers = [];
+    Users.find({}, function(err, allUsers) {
+        if (err) {
+            console.log('error in find users: ', err);
+        }
+        for (i = 0; i < allUsers.length; i++) {
+            phoneNumbers.push(allUsers[i].username);
+        }
+        generateReminders(phoneNumbers);
+    });
+};
+
+var generateReminders = function(recipients) {
+    for (i = 0; i < recipients.length; i++) {
+        var message = randomMessage();
+        console.log(recipients[i] + ' : ' + message);
+    }
+};
+
+sendReminders();
 
 
-module.exports = router;
+
+module.exports = client;
