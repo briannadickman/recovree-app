ef38e79f8a3778d516f2af30094a81142b7bf826
Anisa <anisa.ali7@gmail.com>
2017-06-07 14:04:32 -0500

    get all users from the database

--- server/modules/twilio.js
+++ server/modules/twilio.js
@@ -2,7 +2,11 @@ var express = require('express');
 var cronJob = require('cron').CronJob;
 var Users = require('../models/user');
 var asyncMod = require('async');
-var User = require('../routes/user');
+// var User = require('../routes/user');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var UserModel = require('../models/user');
+var User = mongoose.model('users', UserModel.UserSchema);
 
 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 var twilioNumber = process.env.TWILIO_NUMBER;
