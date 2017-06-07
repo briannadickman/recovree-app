615a933972f1370c09061370bc45dec18e16f99d
Anisa <anisa.ali7@gmail.com>
2017-06-06 23:17:58 -0500

    password reset successful

--- server/modules/twilio.js
+++ server/modules/twilio.js
@@ -21,43 +21,15 @@ var randomIndex = Math.floor(Math.random() * reminderMessage.length);
 var randomomizeReminderMessage = reminderMessage[randomIndex];
 
 function sendSMS(phoneNumber, message) {
-  console.log('WILL SEND THIS NUMBER',phoneNumber );
-  console.log('THIS RESET LINK', message);
-    // client.messages.create({ to: phoneNumber, from: twilioNumber, body: message },
-    //     function(err, data) {
-    //         if(err) {
-    //           console.log('Error Sending Message');
-    //         } else {
-    //           console.log('SMS SENT', data);
-    //         }
-    //     });
+    client.messages.create({ to: phoneNumber, from: twilioNumber, body: message },
+        function(err, data) {
+            if(err) {
+              console.log('Error Sending Message');
+            } else {
+              console.log('SMS SENT', data);
+            }
+        });
 }
-
-console.log('TWILIO',user);
-// router.get('/', function(req, res) {
-//   console.log('FROM RESET PASSWORD');
-// });
-//send out random message to all users
-// var sendReminders = function() {
-//     var phoneNumbers = [];
-//     Users.find({}, function(err, allUsers) {
-//         if (err) {
-//             console.log('error in find users: ', err);
-//         }
-//         for (i = 0; i < allUsers.length; i++) {
-//             phoneNumbers.push(allUsers[i].username);
-//         }
-//         generateReminders(phoneNumbers);
-//     });
-// };
-//
-// //random reminders combined with all user phone numbers
-// var generateReminders = function(recipients) {
-//     for (i = 0; i < recipients.length; i++) {
-//         var message = randomMessage();
-//         // console.log(recipients[i] + ' : ' + message);
-//     }
-// };
 
 
 module.exports = sendSMS;
