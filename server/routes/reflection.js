3585b9c02dd23d898321b24b93d4a48fc76318e7
Anisa <anisa.ali7@gmail.com>
2017-06-09 09:13:20 -0500

    replaces with code from 7 days ago - testing to see if medication bug still persists

--- server/routes/reflection.js
+++ server/routes/reflection.js
@@ -21,201 +21,203 @@ var generateSessionObject = require('../modules/sessionObject');
 var convertCount = require('../modules/convertCount');
 
 ///get reflections from database
-router.get('/', function (req, res) {
-  if(req.isAuthenticated()) {
-  Reflection.find().lean().exec(function(err, reflections){
-    if(err){
-      console.log("Mongo Error: ", err);
-      res.send(500);
+router.get('/', function(req, res) {
+    if (req.isAuthenticated()) {
+        Reflection.find().lean().exec(function(err, reflections) {
+            if (err) {
+                console.log("Mongo Error: ", err);
+                res.send(500);
+            }
+            res.send(reflections);
+        });
+    } else {
+        res.sendStatus(403);
     }
-    res.send(reflections);
-  });
-} else {
-    res.sendStatus(403);
-  }
 });
 
 //admin - return count by day
-router.get('/countByDay', function(req, res){
-  if(req.isAuthenticated()){
-  Reflection.aggregate(
-    [{$group: {
-      _id : { month: {$month : '$reflectionDate'}, day: {$dayOfMonth: '$reflectionDate'}, year: { $year : '$reflectionDate'}},
-      count: { $sum : 1 },
-      }
-    }], function(err, countData){
-    if (err){
-      console.log('error in count by day: ', err);
+router.get('/countByDay', function(req, res) {
+    if (req.isAuthenticated()) {
+        Reflection.aggregate(
+            [{
+                $group: {
+                    _id: { month: { $month: '$reflectionDate' }, day: { $dayOfMonth: '$reflectionDate' }, year: { $year: '$reflectionDate' } },
+                    count: { $sum: 1 },
+                }
+            }],
+            function(err, countData) {
+                if (err) {
+                    console.log('error in count by day: ', err);
+                }
+                var reflectionCountByDate = convertCount(countData);
+                res.send(reflectionCountByDate);
+            });
+    } else {
+        res.sendStatus(403);
     }
-    var reflectionCountByDate = convertCount(countData);
-    res.send(reflectionCountByDate);
-  });
-} else {
-    res.sendStatus(403);
-  }
 });
 
 //async session generation
-router.get('/session/', function(req, res){//took out memberID
-  if (req.isAuthenticated()){
-  var reflections;
-  var medication;
-  var memberID = req.user.memberID;
-  //find reflections by memberID
-  asyncMod.parallel([
-    function(callback){
-      Reflection.find({'memberID': memberID})
-        .sort({'reflectionDate': -1})
-        .exec(function(err, allReflections){
-          if (err){
-            console.log('error in find reflections: ', err);
-            res.sendStatus(500);
-          }
-          sessionOutput = allReflections;
-          callback(null, sessionOutput);
-        });
-    },
-    function(callback, sessionOutput){
-      //does the user have medication?  called in the backend to protect anonymity
-      Registration.findOne({'memberID' : memberID})
-      .select('medication')
-      .exec(function(err, hasMedication){
-        if (err){
-          console.log('error in find meds: ', err);
-        }
-        sessionOutput = hasMedication;
-        callback(null, sessionOutput);
-      });
+router.get('/session/', function(req, res) { //took out memberID
+    if (req.isAuthenticated()) {
+        var reflections;
+        var medication;
+        var memberID = req.user.memberID;
+        //find reflections by memberID
+        asyncMod.parallel([
+                function(callback) {
+                    Reflection.find({ 'memberID': memberID })
+                        .sort({ 'reflectionDate': -1 })
+                        .exec(function(err, allReflections) {
+                            if (err) {
+                                console.log('error in find reflections: ', err);
+                                res.sendStatus(500);
+                            }
+                            sessionOutput = allReflections;
+                            callback(null, sessionOutput);
+                        });
+                },
+                function(callback, sessionOutput) {
+                    //does the user have medication?  called in the backend to protect anonymity
+                    Registration.findOne({ 'memberID': memberID })
+                        .select('medication')
+                        .exec(function(err, hasMedication) {
+                            if (err) {
+                                console.log('error in find meds: ', err);
+                            }
+                            sessionOutput = hasMedication;
+                            callback(null, sessionOutput);
+                        });
+                }
+            ],
+            function(err, sessionOutput) {
+                reflections = sessionOutput[0];
+                medication = sessionOutput[1].medication;
+                //create session object
+                asyncMod.waterfall([
+                        function(callback) {
+                            var serverSessionObject = generateSessionObject(reflections, medication);
+                            callback(null, serverSessionObject);
+                        }
+                    ],
+                    function(err, results) {
+                        var newCount = results.streakCount;
+                        //if reflection(s) exist, save the streak count
+                        if (results.allReflectionsNewToOld[0]) {
+                            Reflection.findOne({ '_id': results.allReflectionsNewToOld[0]._id }, function(err, curReflection) {
+                                if (err) {
+                                    console.log('streak count first reflection update error: ', err);
+                                }
+                                curReflection.streakCount = newCount || curReflection.streakCount;
+                                curReflection.save(function(err, updatedReflection) {
+                                    if (err) {
+                                        console.log('error in reflection put: ', err);
+                                        res.sendStatus(500);
+                                    }
+                                    res.send(results);
+                                });
+                            });
+                        } else {
+                            //send session object defaults for new members
+                            res.send(results);
+                        }
+                    });
+            });
+    } else {
+        console.log('user not authorized');
+        res.sendStatus(403);
     }
-  ],
-  function(err, sessionOutput){
-    reflections = sessionOutput[0];
-    medication = sessionOutput[1].medication;
-    //create session object
-    asyncMod.waterfall([
-      function(callback){
-        var serverSessionObject = generateSessionObject(reflections, medication);
-        callback(null, serverSessionObject);
-      }
-    ],
-    function(err, results){
-      var newCount = results.streakCount;
-      //if reflection(s) exist, save the streak count
-      if (results.allReflectionsNewToOld[0]){
-        Reflection.findOne({'_id' : results.allReflectionsNewToOld[0]._id}, function(err, curReflection){
-          if (err) {
-            console.log('streak count first reflection update error: ', err);
-          }
-          curReflection.streakCount = newCount || curReflection.streakCount;
-          curReflection.save(function(err, updatedReflection){
-            if (err){
-              console.log('error in reflection put: ', err);
-              res.sendStatus(500);
-            }
-            res.send(results);
-          });
-        });
-      } else {
-        //send session object defaults for new members
-        res.send(results);
-      }
-    });
-  });
-  } else {
-    console.log('user not authorized');
-    res.sendStatus(403);
-  }
 });
 
 //initial reflections post
-router.post('/', function(req,res){
-  if(req.isAuthenticated()){
-  var memberID = req.user.memberID;
-  var reflection = req.body;
-  var newReflection = new Reflection({
-    id : req.user._id,
-    date: reflection.reflectionDate,
-    overallfeeling: reflection.overallfeeling,
-    feelings : reflection.feelings,
-    feelingsWhy: reflection.feelingsWhy,
-    drugAlcoholIntake: reflection.drugAlcoholIntake,
-    medication: reflection.medication,
-    sleep: reflection.sleep,
-    dream: reflection.dream,
-    whatDream: reflection.whatDream,
-    exercise: reflection.exercise,
-    food: reflection.food,
-    spnsrMntrConnect: reflection.spnsrMntrConnect,
-    groupMeet: reflection.groupMeet,
-    commntyService: reflection.commntyService,
-    stressors: reflection.stressors,
-    selfishDishonest: reflection.selfishDishonest,
-    howSelfshDishnt: reflection.howSelfshDishnt,
-    tomorrowGoal: reflection.tomorrowGoal,
-    dailyGoal: reflection.dailyGoal,
-    gratitude: reflection.gratitude,
-    peerSupport: reflection.peerSupport,
-    counselor: reflection.counselor,
-    memberID: memberID
-  });
+router.post('/', function(req, res) {
+    if (req.isAuthenticated()) {
+        var memberID = req.user.memberID;
+        var reflection = req.body;
+        var newReflection = new Reflection({
+            id: req.user._id,
+            date: reflection.reflectionDate,
+            overallfeeling: reflection.overallfeeling,
+            feelings: reflection.feelings,
+            feelingsWhy: reflection.feelingsWhy,
+            drugAlcoholIntake: reflection.drugAlcoholIntake,
+            medication: reflection.medication,
+            sleep: reflection.sleep,
+            dream: reflection.dream,
+            whatDream: reflection.whatDream,
+            exercise: reflection.exercise,
+            food: reflection.food,
+            spnsrMntrConnect: reflection.spnsrMntrConnect,
+            groupMeet: reflection.groupMeet,
+            commntyService: reflection.commntyService,
+            stressors: reflection.stressors,
+            selfishDishonest: reflection.selfishDishonest,
+            howSelfshDishnt: reflection.howSelfshDishnt,
+            tomorrowGoal: reflection.tomorrowGoal,
+            dailyGoal: reflection.dailyGoal,
+            gratitude: reflection.gratitude,
+            peerSupport: reflection.peerSupport,
+            counselor: reflection.counselor,
+            memberID: memberID
+        });
 
-  newReflection.save(newReflection, function(err, savedReflection){
-    if(err){
-      console.log("Error: ", err);
-      res.sendStatus(500);
+        newReflection.save(newReflection, function(err, savedReflection) {
+            if (err) {
+                console.log("Error: ", err);
+                res.sendStatus(500);
+            }
+            res.send(savedReflection);
+        });
+    } else {
+        console.log('user not authorized');
+        res.sendStatus(403);
     }
-    res.send(savedReflection);
-  });
-  } else {
-    console.log('user not authorized');
-    res.sendStatus(403);
-  }
 });
 
 //updates reflections at each step of the form
-router.put('/', function (req, res) {
-  if(req.isAuthenticated()){
-  var reflectionUpdate = req.body;
-  Reflection.findOne({'_id' : req.body._id}, function(err, curReflection){
-    if (err) {
-      console.log('reflection put err: ', err);
-      res.sendStatus(500);
-    }
-    //updates reflection found by id or reverts to original if property hasn't been passed
-    curReflection.feelings = reflectionUpdate.feelings || curReflection.feelings;
-    curReflection.feelingsWhy = reflectionUpdate.feelingsWhy || curReflection.feelingsWhy;
-    curReflection.overallfeeling = reflectionUpdate.overallfeeling || curReflection.overallfeeling;
-    curReflection.drugAlcoholIntake = reflectionUpdate.drugAlcoholIntake || curReflection.drugAlcoholIntake;
-    curReflection.medication =  reflectionUpdate.medication || curReflection.medication;
-    curReflection.sleep = reflectionUpdate.sleep || curReflection.sleep;
-    curReflection.dream = reflectionUpdate.dream || curReflection.dream;
-    curReflection.whatDream = reflectionUpdate.whatDream || curReflection.whatDream;
-    curReflection.exercise = reflectionUpdate.exercise || curReflection.exercise;
-    curReflection.food = reflectionUpdate.food || curReflection.food;
-    curReflection.spnsrMntrConnect = reflectionUpdate.spnsrMntrConnect || curReflection.spnsrMntrConnect;
-    curReflection.groupMeet = reflectionUpdate.groupMeet || curReflection.groupMeet;
-    curReflection.commntyService = reflectionUpdate.commntyService || curReflection.commntyService;
-    curReflection.stressors = reflectionUpdate.stressors || curReflection.stressors;
-    curReflection.selfishDishonest = reflectionUpdate.selfishDishonest || curReflection.selfishDishonest;
-    curReflection.howSelfshDishnt = reflectionUpdate.howSelfshDishnt || curReflection.howSelfshDishnt;
-    curReflection.tomorrowGoal = reflectionUpdate.tomorrowGoal || curReflection.tomorrowGoal;
-    curReflection.dailyGoal = reflectionUpdate.dailyGoal || curReflection.dailyGoal;
-    curReflection.gratitude = reflectionUpdate.gratitude || curReflection.gratitude;
-    curReflection.peerSupport = reflectionUpdate.peerSupport || curReflection.peerSupport;
-    curReflection.counselor = reflectionUpdate.counselor || curReflection.counselor;
+router.put('/', function(req, res) {
+    if (req.isAuthenticated()) {
+        var reflectionUpdate = req.body;
+        Reflection.findOne({ '_id': req.body._id }, function(err, curReflection) {
+            if (err) {
+                console.log('reflection put err: ', err);
+                res.sendStatus(500);
+            }
+            //updates reflection found by id or reverts to original if property hasn't been passed
+            curReflection.feelings = reflectionUpdate.feelings || curReflection.feelings;
+            curReflection.feelingsWhy = reflectionUpdate.feelingsWhy || curReflection.feelingsWhy;
+            curReflection.overallfeeling = reflectionUpdate.overallfeeling || curReflection.overallfeeling;
+            curReflection.drugAlcoholIntake = reflectionUpdate.drugAlcoholIntake || curReflection.drugAlcoholIntake;
+            curReflection.medication = reflectionUpdate.medication || curReflection.medication;
+            curReflection.sleep = reflectionUpdate.sleep || curReflection.sleep;
+            curReflection.dream = reflectionUpdate.dream || curReflection.dream;
+            curReflection.whatDream = reflectionUpdate.whatDream || curReflection.whatDream;
+            curReflection.exercise = reflectionUpdate.exercise || curReflection.exercise;
+            curReflection.food = reflectionUpdate.food || curReflection.food;
+            curReflection.spnsrMntrConnect = reflectionUpdate.spnsrMntrConnect || curReflection.spnsrMntrConnect;
+            curReflection.groupMeet = reflectionUpdate.groupMeet || curReflection.groupMeet;
+            curReflection.commntyService = reflectionUpdate.commntyService || curReflection.commntyService;
+            curReflection.stressors = reflectionUpdate.stressors || curReflection.stressors;
+            curReflection.selfishDishonest = reflectionUpdate.selfishDishonest || curReflection.selfishDishonest;
+            curReflection.howSelfshDishnt = reflectionUpdate.howSelfshDishnt || curReflection.howSelfshDishnt;
+            curReflection.tomorrowGoal = reflectionUpdate.tomorrowGoal || curReflection.tomorrowGoal;
+            curReflection.dailyGoal = reflectionUpdate.dailyGoal || curReflection.dailyGoal;
+            curReflection.gratitude = reflectionUpdate.gratitude || curReflection.gratitude;
+            curReflection.peerSupport = reflectionUpdate.peerSupport || curReflection.peerSupport;
+            curReflection.counselor = reflectionUpdate.counselor || curReflection.counselor;
 
-    curReflection.save(function(err, updatedReflection){
-      if (err){
-        console.log('error in reflection put: ', err);
-        res.sendStatus(500);
-      }
-      res.send(updatedReflection);
-    });
-  });
-  } else {
-    console.log('user not authorized');
-    res.sendStatus(403);
-  }
+            curReflection.save(function(err, updatedReflection) {
+                if (err) {
+                    console.log('error in reflection put: ', err);
+                    res.sendStatus(500);
+                }
+                res.send(updatedReflection);
+            });
+        });
+    } else {
+        console.log('user not authorized');
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
