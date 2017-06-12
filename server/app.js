5a8ecb93039dbcf74cd542a06273c31762726387
Anisa <anisa.ali7@gmail.com>
2017-06-12 12:31:31 -0500

    remove space

--- server/app.js
+++ server/app.js
@@ -22,19 +22,18 @@ var twilio = require('./modules/twilio.js');
 
 // Body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve back static files
 app.use(express.static('./server/public'));
 
 // Passport Session Configuration //
-
 app.use(session({
-   secret: process.env.PASSPORT_SECRET,
-   key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
-   resave: 'true',
-   saveUninitialized: false,
-   cookie: { maxage: 600000, secure: false }
+    secret: process.env.PASSPORT_SECRET,
+    key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
+    resave: 'true',
+    saveUninitialized: false,
+    cookie: { maxage: 600000, secure: false }
 }));
 
 // start up passport sessions
@@ -53,6 +52,6 @@ app.use('/', index);
 app.set('port', (process.env.PORT || 5000));
 
 // Listen //
-app.listen(app.get("port"), function(){
-   console.log("Listening on port: " + app.get("port"));
+app.listen(app.get("port"), function() {
+    console.log("Listening on port: " + app.get("port"));
 });
