eb9023c1a601272e8709fc891081320d1986267e
Anisa <anisa.ali7@gmail.com>
2017-06-14 11:58:23 -0500

    saved time in reflectionSchema as LocalString, fixed issue with saving as UTC instead of CST

--- server/models/reflection.js
+++ server/models/reflection.js
@@ -1,31 +1,34 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var date = new Date();
+var newdate = date.toLocaleString();
+
 var ReflectionSchema = mongoose.Schema({
-  feelings: {type: Array},
-  overallfeeling: {type: Number},
-  feelingsWhy: {type: String},
-  drugAlcoholIntake: {type: Boolean},
-  medication: {type: Boolean},
-  sleep: {type: Number},
-  dream: {type: Boolean},
-  whatDream: {type: String},
-  exercise: {type: Number},
-  food: {type: Number},
-  spnsrMntrConnect: {type: Boolean},
-  groupMeet: {type: Number},
-  commntyService: {type: Boolean},
-  stressors: {type: Array},
-  selfishDishonest: {type: Boolean},
-  howSelfshDishnt: {type: String},
-  tomorrowGoal: {type: String},
-  dailyGoal: {type: Boolean},
-  gratitude: {type: String},
-  peerSupport: {type: Boolean},
-  counselor: {type: Boolean},
-  streakCount : {type: Number, default: 0},
-  reflectionDate: {type: Date, default: Date.now},
-  memberID: {type: Number, ref: 'Registration'} //references Registration Schema
+    feelings: { type: Array },
+    overallfeeling: { type: Number },
+    feelingsWhy: { type: String },
+    drugAlcoholIntake: { type: Boolean },
+    medication: { type: Boolean },
+    sleep: { type: Number },
+    dream: { type: Boolean },
+    whatDream: { type: String },
+    exercise: { type: Number },
+    food: { type: Number },
+    spnsrMntrConnect: { type: Boolean },
+    groupMeet: { type: Number },
+    commntyService: { type: Boolean },
+    stressors: { type: Array },
+    selfishDishonest: { type: Boolean },
+    howSelfshDishnt: { type: String },
+    tomorrowGoal: { type: String },
+    dailyGoal: { type: Boolean },
+    gratitude: { type: String },
+    peerSupport: { type: Boolean },
+    counselor: { type: Boolean },
+    streakCount: { type: Number, default: 0 },
+    reflectionDate: { type: Date, default: newdate },
+    memberID: { type: Number, ref: 'Registration' } //references Registration Schema
 });
 
 
