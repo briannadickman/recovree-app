713e28b1e65c024695eda40b7b0270ba1e557c66
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-06-30 16:15:12 -0500

    html

--- client/scripts/factories/UserService.js
+++ client/scripts/factories/UserService.js
@@ -54,6 +54,7 @@ myApp.factory('UserService', ['$http', '$location', function($http, $location) {
 
 
     function userDemographics(registration) {
+        console.log("userDemographics registration",registration);
         $http.post('/register/registration', registration).then(function(response) {
             emptyDemographics(registration);
             $location.path('/login');
